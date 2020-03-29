6fbb85cf01b0bf2147b15f22e16fc56819a940dc
Miguel Vera <miguel.vera7908@gmail.com>
2020-03-29 11:32:10 -0500

    update to repo

---
+++ src/main/java/com/pantrychef/pantrychef/PantrychefApplication.java
@@ -0,0 +1,13 @@
+package com.pantrychef.pantrychef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PantrychefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PantrychefApplication.class, args);
+	}
+
+}
