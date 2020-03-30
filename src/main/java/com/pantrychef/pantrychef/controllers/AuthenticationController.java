814edc9676be2149dd7781f24e2a25354e7b4490
Matthew Hettler <Matthew.R.Hettler@gmail.com>
2020-03-30 11:53:31 -0500

    Added a logout method and a sign-up link on the navbar partial.

--- src/main/java/com/pantrychef/pantrychef/controllers/AuthenticationController.java
+++ src/main/java/com/pantrychef/pantrychef/controllers/AuthenticationController.java
@@ -2,11 +2,18 @@ package com.pantrychef.pantrychef.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "users/login";
+    }
+
+    @GetMapping("/logout")
+    @ResponseBody
+    public String showLogoutForm() {
+        return "You have been logged out!";
     }
 }
