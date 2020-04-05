4a04a6d6677449c14fd7a1392d632cdb4d969e10
Miguel Vera <miguel.vera7908@gmail.com>
2020-04-05 16:39:15 -0500

    added ingredients table to accept multiple ingredients when creating a recipe

---
+++ src/main/resources/static/js/scripts.js
@@ -0,0 +1,15 @@
+const attachAddSkillEventListener = function() {
+    if (document.getElementById("add-ingredient-button") !== null) {
+        let i = 0;
+        document.getElementById("add-ingredient-button").addEventListener("click", function (e) {
+            e.preventDefault();
+            let newDiv = document.createElement("div");
+            // and give it some content
+            newDiv.innerHTML = `<input class = "form-control" type="text" id="ingredients-param-${i}" name="ingredient-param" placeholder="Please enter an ingredient.">`
+            // add the text node to the newly created div
+            document.querySelector("#ingredient-div").appendChild(newDiv);
+            i++;
+        })
+    }
+}
+attachAddSkillEventListener();
